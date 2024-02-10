1b8d6a7931a3fad7af3d0a08610f6d6bef7c5790
Joakim Bjärkstedt <joakim198904@live.se>
2024-02-10 18:52:17 +0100

    almost done with my notepad

--- app/src/main/java/com/example/idun/MainActivity.kt
+++ app/src/main/java/com/example/idun/MainActivity.kt
@@ -1,13 +1,5 @@
 package com.example.idun
-/*  #100DaysOfCode
-    Day
-    1/100: Struggled to implement the button in previous version 
-    2/100: Decided to restart the project and ditch the fragment part for now and work mainly with activites. created the layout for the MainActivity.
-    3/100: Implemented buttons with placeholder "toast" Messages until proper functionalities are in place.
-    4/100: Implemented a NotepadDataManager and Custom Adapter to my ShoppingListActivity to manage saving procedure to SharedPreferences,
-    Not finished. need to implement the button to add items in the list and amount of items.
 
-*/
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/example/idun/NotepadActivity.kt
+++ app/src/main/java/com/example/idun/NotepadActivity.kt
@@ -5,19 +5,22 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.idun.databinding.ActivityNotepadBinding
 
 class NotepadActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNotepadBinding
-    private var titleEditText: EditText? = null
-    private var contentEditText: EditText? = null
-    private var adapter: NotesAdapter? = null
-    var saveButton: Button? = null
-    var deleteButton: Button? = null
-    private var notepadDataManager: NotepadDataManager? = null
+    private lateinit var titleEditText: EditText
+    private lateinit var contentEditText: EditText
+    private lateinit var adapter: NotesAdapter
+    private lateinit var listView: ListView
+    lateinit var saveButton: Button
+    lateinit var deleteButton: Button
+    private lateinit var notepadDataManager: NotepadDataManager
     private var editNotePosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNotepadBinding.inflate(layoutInflater)
@@ -27,10 +30,10 @@ class NotepadActivity : AppCompatActivity() {
 
         adapter = NotesAdapter(this, notepadDataManager.getNotes(), notepadDataManager)
         titleEditText = findViewById(R.id.et_titlenote)
-        contentEditText = findViewById<EditText>(R.id.et_contentnote)
-        saveButton = findViewById<Button>(R.id.btn_addNote)
-
-        deleteButton = findViewById<Button>(R.id.btn_deleteNote)
+        contentEditText = findViewById(R.id.et_contentnote)
+        saveButton = findViewById(R.id.btn_addNote)
+        listView = findViewById(R.id.lv_notesList)
+        deleteButton = findViewById(R.id.btn_deleteNote)
         val notes: List<String> = notepadDataManager.getNotes()
 
         val savedTitle: String = notepadDataManager.getSavedTitle()
@@ -51,6 +54,7 @@ class NotepadActivity : AppCompatActivity() {
                     titleEditText.setText(noteParts[0])
                     contentEditText.setText(noteParts[1])
                 }
+                adapter.notifyDataSetChanged()
             }
         }
 
@@ -58,13 +62,18 @@ class NotepadActivity : AppCompatActivity() {
             val title: String = titleEditText.getText().toString()
             val content: String = contentEditText.getText().toString()
             if (title.isEmpty()) {
-                Toast.makeText(this@NotepadActivity, R.string.save_error_message, Toast.LENGTH_SHORT)
+                Toast.makeText(
+                    this@NotepadActivity,
+                    R.string.save_error_message,
+                    Toast.LENGTH_SHORT
+                )
                     .show()
             } else {
                 if (editNotePosition != -1) {
                     val isUpdated: Boolean =
                         notepadDataManager.updateNoteByPosition(editNotePosition, title, content)
                     if (isUpdated) {
+                        adapter.notifyDataSetChanged()
                         val intent = Intent(
                             this@NotepadActivity,
                             MainActivity::class.java
@@ -72,12 +81,13 @@ class NotepadActivity : AppCompatActivity() {
                         startActivity(intent)
                     }
                 } else {
-                    val newPosition: Int = notepadDataManager.getNotes().size()
+                    val newPosition: Int = notepadDataManager.getNotes().size
                     notepadDataManager.saveNote(
                         newPosition,
                         title,
                         content
                     ) //Troligtvis här problemet ligger, tilldelar endast "Key" 0 om och om igen.
+                    adapter.notifyDataSetChanged()
                     val intent = Intent(
                         this@NotepadActivity,
                         MainActivity::class.java
@@ -90,7 +100,8 @@ class NotepadActivity : AppCompatActivity() {
         val deleteButton = findViewById<Button>(R.id.btn_deleteNote)
         deleteButton.setOnClickListener {
             if (editNotePosition != -1) {
-                NotesAdapter.deleteNoteAtPosition(editNotePosition)
+                adapter.deleteNoteAtPosition(editNotePosition)
+                adapter.notifyDataSetChanged()
                 val intent = Intent(
                     this@NotepadActivity,
                     MainActivity::class.java
@@ -104,4 +115,3 @@ class NotepadActivity : AppCompatActivity() {
 }
 
 
-}
